a7fbbf242cb2840213daed3dfb8b590885b89e2f
trevor-shoe <trevorschupbach@gmail.com>
2025-12-07 14:21:14 -0500

    stbi from buffer

--- src/include/sndx/render/image/stbimage.hpp
+++ src/include/sndx/render/image/stbimage.hpp
@@ -40,6 +40,28 @@ namespace sndx::render {
 
 			return ImageData{ size_t(width), size_t(height), channels, std::move(dat) };
 		}
+
+		[[nodiscard]]
+		std::optional<ImageData> loadFromBuffer(std::span<stbi_uc> data, uint8_t channels) const noexcept {
+			stbi_set_flip_vertically_on_load_thread(m_flip);
+
+			int width, height, chan;
+			auto img = stbi_load_from_memory(data.data(), static_cast<int>(data.size()), &width, &height, &chan, channels);
+			auto bdata = reinterpret_cast<std::byte*>(img);
+
+			if (!bdata)
+				return std::nullopt;
+
+			std::vector<std::byte> dat{};
+			dat.reserve(width * height * channels);
+
+			std::copy_n(bdata, width * height * channels, std::back_inserter(dat));
+			stbi_image_free(bdata);
+
+			return ImageData{ size_t(width), size_t(height), channels, std::move(dat) };
+		}
+
+		std::optional<ImageData> loadFromBuffer(std::nullptr_t data, uint8_t channels) const = delete;
 	};
 
 	class STBimageSaver {
@@ -57,13 +79,16 @@ namespace sndx::render {
 			std::u8string strPath{path.u8string()};
 			auto cstr = reinterpret_cast<const char*>(strPath.c_str());
 
+			int width = static_cast<int>(image.width());
+			int height = static_cast<int>(image.height());
+
 			if (path.extension() == ".jpg" || path.extension() == ".jpeg")
-				return stbi_write_jpg(cstr, image.width(), image.height(), image.channels(), image.data(), m_quality);
+				return stbi_write_jpg(cstr, width, height, image.channels(), image.data(), m_quality);
 			
 			if (path.extension() == ".bmp")
-				return stbi_write_bmp(cstr, image.width(), image.height(), image.channels(), image.data());
+				return stbi_write_bmp(cstr, width, height, image.channels(), image.data());
 			
-			return stbi_write_png(cstr, image.width(), image.height(), image.channels(), image.data(), 0);
+			return stbi_write_png(cstr, width, height, image.channels(), image.data(), 0);
 		}
 	};
 }
